4860dc1bc1289520b6b745874320d9b7d0b69683
Alexander <121974196+Lenchis21@users.noreply.github.com>
2026-03-21 16:11:24 +0300

    Добавьте файлы проекта.

---
+++ DjangoSite/app/views.py
@@ -0,0 +1,54 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def home(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Домашняя страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Ваша страница контактов.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Страница описания вашего приложения.',
+            'year':datetime.now().year,
+        }
+    )
+
+def link(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/link.html',
+        {
+            'title':'Полезные ссылки',
+            'message':'Страница описания вашего приложения.',
+            'year':datetime.now().year,
+        }
+    )
